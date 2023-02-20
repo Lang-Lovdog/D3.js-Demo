f27243def971056f4e7822aecb71eea07f97d0ba
Brandon Marquez Salazar <Lang-Lovdog>
2023-02-20 10:08:33 -0600

    Ya se mapean los datos

--- DataVisualization01/d3Demo.js
+++ DataVisualization01/d3Demo.js
@@ -1,6 +1,6 @@
 d3.select("div").style("color","white").style("text-shadow","0 0 1px black, 0 0 1.4px black, 0 0 1.6px black, 0 0 1.8px black,0 0 1.10px black")
 d3.select("div").style("background-image","linear-gradient(20deg, gray,black)");
-d3.select("body").style("background-color","black");
+d3.select("body").style("background-color","black").style("color","white");
 /*
 var svg = d3.select('div').append('svg');
 svg
@@ -15,7 +15,7 @@ svg
   .attr('cy','20%')
   .attr
 */
-
+/*
 const data= [35,15,15,35]
 
 const cosa=d3.select('div')
@@ -34,7 +34,7 @@ const arc = d3.arc()
   .outerRadius(radius)
   .innerRadius(15)
 /*  .startAngle(0)
-  .endAngle(Math.PI)*/
+  .endAngle(Math.PI)
 
 const g = svg.selectAll('.arc')
   .data(pie(data))
@@ -47,3 +47,46 @@ g.append('path')
   .style('fill',(d,i)=>colorScale(i))
   .style('stroke','#221243')
   .style('stroke-width',3)
+*/
+
+
+// Tratamiento de los datos obtenidos desde un CSV
+// Para comenzar, los datos del CSV se obtinen con
+// el método d3.csv(<filename>), y se almacenarán
+// en una vaiable constante.
+
+var AlumniData= [];
+
+const recarga = function(){
+  d3.csv("info_students.csv",d=>{
+    AlumniData.push(d);
+  }).then(function(){
+    redibuja()
+  });
+}
+
+
+/*d3.select("#CSV_DUMP")
+  .data(AlumniData)
+  .text(d=>{return d});
+
+
+d3.select("#Tabla0Alumni")
+  .selectAll("tr")
+  .data(AlumniData)
+  .enter()
+  .append("tr")
+  .text(d=>{
+    return d;
+  })*/
+
+var redibuja = function (){
+  console.log("Dibujando")
+  AlumniData.map(i=>{
+    if(i.pet===null || i.pet===undefined || i.pet===""){
+      console.log(i);
+    }
+  })
+};
+
+recarga();
