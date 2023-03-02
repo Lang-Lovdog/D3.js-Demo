9d1e8d1d618074343e3f21d9b1f5eebab16460b9
Brandon Marquez Salazar <Lang-Lovdog>
2023-03-02 00:11:11 -0600

    Graficas exitosas uwu

--- DataVisualization02/TratamientoDatos.js
+++ DataVisualization02/TratamientoDatos.js
@@ -17,8 +17,14 @@ const recarga = function(elArchivo){
     laData.push(d);
   }).then(()=>{
 
+
+    GraficaHistogramaKey(laData,"bus_ter","#demoProfe",true);
+    GraficaHistogramaKey(laData,"crime_rate","#demoProfe0",true);
+    GraficaHistogramaKey(laData,"price","#demoProfe1",true);
+    ScatterPlot(laData,"#scatterplot","crime_rate","price");
+    ScatterPlot(laData,"#scatterplot","price","crime_rate");
     DefineNull(laData,"?","workclass","Private");
-    const freq_ednum=frecuencia(laData,"education.num");
+    /*const freq_ednum=frecuencia(laData,"education_num");
     const freq_edad=frecuencia(laData,"age");
     const freq_income=frecuencia(laData,"income");
     const freq_workclass=frecuencia(laData,"workclass");
@@ -26,7 +32,7 @@ const recarga = function(elArchivo){
     histogram_freq(freq_income,"#ElIncome","Ingresos (Sueldo)",[20,100,100],[100,250,120],true);
     histogram_freq(freq_edad,"#EdadesOrig","Edades",[0,150,150],[0,230,255]);
     histogram_freq(freq_workclass,"#WorkClassOrig","WorkClass",[200,1,220],[201,0,230],true);
-    ScatterPlot(laData,"#scatterplot","age","hours.per.week","Relación de edad y Horas de trabajo");
+    ScatterPlot(laData,"#scatterplot","age",'education_num',"Relación de edad y Horas de trabajo");*/
     //console.log(Object.keys(laData['0']))
   });
 }
@@ -37,6 +43,20 @@ const DefineNull = (Arreglo,ClaveDeNull,Key,NewValue)=>{
       item[Key]=NewValue;
     }
   })
+}
+
+const ArregloKey = (Arreglo,Key)=>{
+  var ArregloSalida=new Array();
+  Arreglo.map(item=>{
+    ArregloSalida.push(parseFloat(item[Key]));
+  })
+  console.log(ArregloSalida);
+  return ArregloSalida;
+}
+
+const GraficaHistogramaKey = (LosDatos, LaKey,LeDiv,Porcentual=false)=>{
+  const freq=frecuencia(LosDatos,LaKey);
+  histogram_freq(freq,LeDiv,LaKey,[0,0,0],[127,127,127],Porcentual);
 }
 
 const frecuencia = (Arreglo,Key)=>{
@@ -53,7 +73,7 @@ const frecuencia = (Arreglo,Key)=>{
   aux_array.forEach(item=>{
     histArray[item]+=1;
   })
-  console.log(histArray)
+  //console.log(histArray)
   return histArray;
 }
 
@@ -61,9 +81,9 @@ const frecuencia = (Arreglo,Key)=>{
 const histogram_freq = (tabla_frequencias,leDiv,leTitle, ColorInit=[0,0,0],ColorIniv=[255,255,255],porcentual=false)=>{
   var proporcion;
   const maxValue=Object.values(tabla_frequencias);
-  console.log(maxValue);
+  //console.log(maxValue);
   i=d3.max(maxValue);
-    console.log(i);
+    //console.log(i);
     if(porcentual){
       proporcion=100/i;
     }else if(i>999999){
@@ -79,10 +99,10 @@ const histogram_freq = (tabla_frequencias,leDiv,leTitle, ColorInit=[0,0,0],Color
     }else if(i>9){
       proporcion=1;
     }
-  console.log(proporcion);
+  //console.log(proporcion);
   WidTh=100/Object.keys(tabla_frequencias).length;
-  console.log(Object.keys(tabla_frequencias).length);
-  console.log(WidTh);
+  //console.log(Object.keys(tabla_frequencias).length);
+  //console.log(WidTh);
 
   leDivTitulo=leDiv+"Titulo";
   leDivCuerpo=leDiv+"Cuerpo";
@@ -91,7 +111,7 @@ const histogram_freq = (tabla_frequencias,leDiv,leTitle, ColorInit=[0,0,0],Color
   //Object.keys(tabla_frequencias).map(elemento=>{
     //console.log(elemento);
     //console.log(tabla_frequencias[elemento]);
-    console.log("___");
+    //console.log("___");
   //})
   const Clases=Object.keys(tabla_frequencias);
   // Le decimos sobre qué elemento vamos a operar
@@ -190,7 +210,7 @@ const histogram_freq = (tabla_frequencias,leDiv,leTitle, ColorInit=[0,0,0],Color
       lBlu=d3.randomUniform(ColorInit[2],ColorIniv[2])();
       return 'rgb('+lRed+','+lGre+','+lBlu+')'
       })
-    .style("margin",'2px')
+    .style("margin",'.1px')
     .style('width',WidTh+'%')
   // En esta parte, el ancho se relaciona al valor de interés
     .style('height',d=>{
@@ -234,32 +254,88 @@ const ScatterPlot = (LosDatos,leDiv,ValorKeyX,ValorKeyY,Titulo)=>{
     ]
   }) Para la posteridad*/
 
-  const LeSctatterPlotain=d3.select(leDiv)
-    .append("svg")
-    .append("g");
+  var margin = {top: 10, right: 30, bottom: 30, left: 60},
+    width = 460 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
 
-  // Add X axis
+
+  laTablaId=leDiv+"Tabla";
+  d3.select(leDiv)
+    .append("table")
+    .attr("id",laTablaId.slice(1))
+
+  d3.select(laTablaId)
+  .append("tr")
+  .append("td")
+  .append("div")
+  .text(ValorKeyY)
+  .style("transform","rotate(-90deg)");
+
+  var LeSctatterPlotain = d3.select(laTablaId).select("tr")
+  .append("td")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+  
+  /*MaxX=d3.max(Object.values( frecuencia(LosDatos,ValorKeyX) ))
+  MaxY=d3.max(Object.values( frecuencia(LosDatos,ValorKeyY) ))*/
+
+  const ValoresX=ArregloKey(LosDatos,ValorKeyX);
+  const ValoresY=ArregloKey(LosDatos,ValorKeyY);
+  var MaxX=d3.max(ValoresX)+1;
+  var MaxY=d3.max(ValoresY)+1;
+  //console.log(MaxX);
+  //console.log(MaxY);
+
+   // Add X axis
   var x = d3.scaleLinear()
-    .domain([0, 4000])
-    .range([ 0, "300px" ]);
-  LeSctatterPlotain.append("g")
-    .attr("transform", "translate(0,300px)")
-    .call(d3.axisBottom(x));
+    .domain([0, MaxX])
+    .range([ 0, width ]);
 
   // Add Y axis
   var y = d3.scaleLinear()
-    .domain([0, 500000])
-    .range([ "300px", 0]);
+    .domain([0, MaxY])
+    .range([ height, 0]);
+
+  IdX=leDiv+"labelX";
+  IdY=leDiv+"labelY";
+
   LeSctatterPlotain.append("g")
+    .attr("id",IdX.slice(1))
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
+
+  LeSctatterPlotain.append("g")
+    .attr("id",IdY.slice(1))
     .call(d3.axisLeft(y))
-  
+
+  d3.select(IdX)
+    .append("g")
+    .attr("class","tick")
+    .attr("transform",width+",0")
+    .append("text")
+    .text("income per capita, inflation-adjusted (dollars)");
+
+// Add dots
   LeSctatterPlotain.append('g')
     .selectAll("dot")
     .data(LosDatos)
     .enter()
     .append("circle")
-      .attr("cx", function (d) { return x(d[ ValorKeyX ]); } )
-      .attr("cy", function (d) { return y(d[ ValorKeyY ]); } )
-      .attr("r","1.5px")
-      .style("fill", "#69b3a2");
+      .attr("cx", function (d) { return x(d[ValorKeyX]); } )
+      .attr("cy", function (d) { return y(d[ValorKeyY]); } )
+      .attr("r", 1.5)
+      .style("fill", "#660000")
+
+  const elLastTR=d3.select(laTablaId)
+    .append("tr")
+  elLastTR
+    .append("td")
+  elLastTR
+    .append("td")
+    .style("text-align","center")
+    .text(ValorKeyX);
 }
